dfc22667bd57eba805cd4b999b1a51bf05ed67dd
Ilya Titkov <ilya@titkov.me>
2020-12-12 10:45:28 +0300

    Add config

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
 # Unblock
 Приложение, которое работает как dns proxy и прописывает статические маршруты роутеру keenetic для ip адресов, оказавшихся в [дампе роскомнадзора](https://github.com/zapret-info/z-i).
 
-Переменные окружения для запуска:
-- UNBLOCK_BIND_ADDR - ip-port для dns сервера (0.0.0.0:53) 
-- UNBLOCK_DNS_UPSTREAM - ip-port dns upstream сервера (8.8.8.8:53)
-- UNBLOCK_BLACKLIST_DUMP - ссылка на дамп  (https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv)
-- UNBLOCK_ROUTER_API - ссылка на апи роутера без аутентификации (192.168.1.1:79)
-- UNBLOCK_ROUTE_INTERFACE - название интерфейса, через который будет маршрутизироваться заблокированный ip адрес (OpenVPN1)
-- UNBLOCK_BLACKLIST_UPDATE_INTERVAL_SEC - период обновления дампа в секундах (3600)
+config.example.yml - пример конфига
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use std::{net::SocketAddr, sync::Arc, time::Duration};
+use std::{
+    collections::HashSet,
+    net::{Ipv4Addr, SocketAddr},
+    sync::Arc,
+    time::Duration,
+};
 
 use anyhow::Result;
 use dns::client::{CachedClient, UdpClient};
 use log::info;
 use routers::KeeneticClient;
+use serde::Deserialize;
+use tokio::stream::StreamExt;
 use unblock::Unblocker;
 
 mod blacklist;
@@ -13,24 +20,44 @@ mod dns_handler;
 mod routers;
 mod unblock;
 
+#[derive(Deserialize)]
+struct Config {
+    bind_addr: SocketAddr,
+    dns_upstream: SocketAddr,
+    blacklist_dump_uri: String,
+    #[serde(with = "serde_humantime")]
+    blacklist_update_interval: Duration,
+    router_api_uri: String,
+    route_interface: String,
+    manual_whitelist: HashSet<Ipv4Addr>,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
-    let bind_addr: SocketAddr = std::env::var("UNBLOCK_BIND_ADDR")?.parse()?;
-    let dns_upstream: SocketAddr = std::env::var("UNBLOCK_DNS_UPSTREAM")?.parse()?;
-    let blacklist_dump = std::env::var("UNBLOCK_BLACKLIST_DUMP")?.parse()?;
-    let router_api = std::env::var("UNBLOCK_ROUTER_API")?.parse()?;
-    let route_interface = std::env::var("UNBLOCK_ROUTE_INTERFACE")?;
-    let blacklist_update_interval_s =
-        Duration::from_secs(std::env::var("UNBLOCK_BLACKLIST_UPDATE_INTERVAL_SEC")?.parse()?);
-
-    let router_client = KeeneticClient::new(router_api, route_interface);
-    let blacklists =
-        blacklist::create_blacklists_stream(blacklist_dump, blacklist_update_interval_s).await?;
+    let config_name = std::env::args()
+        .nth(1)
+        .expect("Config file should be specified as first argument");
+    let mut settings = config::Config::default();
+    settings.merge(config::File::with_name(&config_name))?;
+    let config = settings.try_into::<Config>()?;
+    let router_client = KeeneticClient::new(config.router_api_uri.parse()?, config.route_interface);
+    let blacklists = blacklist::create_blacklists_stream(
+        config.blacklist_dump_uri.parse()?,
+        config.blacklist_update_interval,
+    )
+    .await?;
+    let manual = config.manual_whitelist;
+    let blacklists = blacklists.map(move |mut blacklist| {
+        blacklist.extend(manual.iter().copied());
+        blacklist
+    });
     let unblocker = Arc::new(Unblocker::new(blacklists, router_client).await?);
-    let dns_client = Arc::new(CachedClient::new(UdpClient::new(dns_upstream).await?));
+    let dns_client = Arc::new(CachedClient::new(
+        UdpClient::new(config.dns_upstream).await?,
+    ));
     let dns_handle = tokio::spawn(
-        dns::server::create_udp_server(bind_addr, move |query| {
+        dns::server::create_udp_server(config.bind_addr, move |query| {
             let unblocker = unblocker.clone();
             let dns_client = dns_client.clone();
             dns_handler::handle_query(query, unblocker, dns_client)
