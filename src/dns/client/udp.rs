0274378819e98b6dfa800fccc8e76ece5223f0e8
Ilya Titkov <ilya@titkov.me>
2021-02-02 23:08:50 +0300

    Update udp client

--- src/dns/client/udp.rs
+++ src/dns/client/udp.rs
@@ -1,8 +1,11 @@
-use std::{collections::HashMap, net::SocketAddr, sync::Arc};
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    net::SocketAddr,
+    sync::Arc,
+};
 
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
-use bytes::Bytes;
 use log::error;
 use tokio::{
     net::UdpSocket,
@@ -11,7 +14,7 @@ use tokio::{
         oneshot,
     },
 };
-use tokio_stream::{wrappers::UnboundedReceiverStream, StreamExt};
+use tokio_stream::StreamExt;
 
 use crate::dns::{
     create_udp_dns_stream,
@@ -21,7 +24,7 @@ use crate::dns::{
 use super::DnsClient;
 
 pub struct UdpClient {
-    responses: UnboundedSender<ResponseWaiter>,
+    responses: UnboundedSender<Request>,
 }
 
 impl UdpClient {
@@ -40,8 +43,8 @@ impl DnsClient for UdpClient {
     async fn send(&self, query: Query) -> Result<Response> {
         let (response_tx, response_rx) = oneshot::channel();
         self.responses
-            .send(ResponseWaiter {
-                request: query,
+            .send(Request {
+                query,
                 waiter: response_tx,
             })
             .expect("Receiver dropped");
@@ -50,64 +53,80 @@ impl DnsClient for UdpClient {
 }
 
 #[derive(Debug)]
-enum ClientMessage {
-    Waiter(ResponseWaiter),
-    Response(Result<Bytes, tokio::io::Error>),
-}
-
-#[derive(Debug)]
-struct ResponseWaiter {
-    request: Query,
+struct Request {
+    query: Query,
     waiter: oneshot::Sender<Result<Response>>,
 }
 
-async fn responses_handler(socket: UdpSocket, waiters: UnboundedReceiver<ResponseWaiter>) {
+async fn responses_handler(socket: UdpSocket, mut requests: UnboundedReceiver<Request>) {
     let socket = Arc::new(socket);
-    let mut msgs = Box::pin(
-        create_udp_dns_stream(socket.clone())
-            .map(|r| r.map(|(_, b)| b))
-            .map(ClientMessage::Response),
-    )
-    .merge(UnboundedReceiverStream::new(waiters).map(ClientMessage::Waiter));
-    let mut waiters = HashMap::new();
-    loop {
-        let handle_response = async {
-            match msgs.next().await.expect("Should be infinite") {
-                ClientMessage::Waiter(waiter) => {
-                    let request = waiter.request.clone();
-                    if let Some(prev_request) = waiters.insert(waiter.request.header().id, waiter) {
-                        let _ = prev_request.waiter.send(Err(anyhow!(
-                            "Dublicate request id. Previous request: {:?}. Current request: {:?}",
-                            prev_request.request.parse(),
-                            request.parse()
-                        )));
-                    }
+    let mut udp_stream = Box::pin(create_udp_dns_stream(socket.clone()));
+    let mut waiting_requests = HashMap::new();
 
-                    socket.send(&request.bytes()).await?;
-                }
-                ClientMessage::Response(response) => {
-                    let response = Response::from_bytes(response?)?;
-                    if let Some(waiter) = waiters.remove(&response.header().id) {
-                        let response = (|| {
-                            let parsed_request = waiter.request.parse()?;
-                            let request_domains = parsed_request.domains();
-                            let parsed_response = response.parse()?;
-                            let response_domains = parsed_response.domains();
-                            if request_domains.eq(response_domains) {
-                                Ok(response)
-                            } else {
-                                Err(anyhow!("Request and response domains don't match. Request: {:?}. Response: {:?}", parsed_request, parsed_response))
-                            }
-                        })();
-                        let _ = waiter.waiter.send(response);
+    loop {
+        let handler = async {
+            tokio::select! {
+                    Some(request) = requests.recv() => {
+                        process_request(&mut waiting_requests, &socket, request).await;
                     }
+                    Some(response) = udp_stream.next() => {
+                        let (_, response) = response?;
+                        let response = Response::from_bytes(response)?;
+                        process_response(&mut waiting_requests, response).await;
+
                 }
             }
             Ok::<_, anyhow::Error>(())
         };
-        if let Err(err) = handle_response.await {
+        if let Err(err) = handler.await {
             error!("Got error while handling dns message: {:#}", err);
         }
+    }
+}
+
+async fn process_request(
+    waiting_requests: &mut HashMap<u16, Request>,
+    socket: &UdpSocket,
+    request: Request,
+) {
+    if let Err(err) = socket.send(&request.query.bytes()).await {
+        let _ = request.waiter.send(Err(err.into()));
+    } else {
+        match waiting_requests.entry(request.query.header().id) {
+            Entry::Occupied(mut prev_request) => {
+                let err = Err(anyhow!(
+                    "Dublicate request id. Previous query: {:?}. Current query: {:?}",
+                    prev_request.get().query.parse(),
+                    request.query.parse()
+                ));
+                let prev_request = prev_request.insert(request);
+                let _ = prev_request.waiter.send(err);
+            }
+            Entry::Vacant(x) => {
+                x.insert(request);
+            }
+        }
+    }
+}
+
+async fn process_response(waiting_requests: &mut HashMap<u16, Request>, response: Response) {
+    if let Some(request) = waiting_requests.remove(&response.header().id) {
+        let response = (|| {
+            let parsed_query = request.query.parse()?;
+            let query_domains = parsed_query.domains();
+            let parsed_response = response.parse()?;
+            let response_domains = parsed_response.domains();
+            if query_domains.eq(response_domains) {
+                Ok(response)
+            } else {
+                Err(anyhow!(
+                    "Request and response domains don't match. Query: {:?}. Response: {:?}",
+                    parsed_query,
+                    parsed_response
+                ))
+            }
+        })();
+        let _ = request.waiter.send(response);
     }
 }
 
