0e12241f6acb1492419463bd947e88d9d4bb53ee
Ilya Titkov <ilya@titkov.me>
2021-09-26 19:05:18 +0300

    Bump fure

--- src/dns/client/retry.rs
+++ src/dns/client/retry.rs
@@ -4,7 +4,7 @@ use super::DnsClient;
 use crate::dns::message::{Query, Response};
 use anyhow::Result;
 use async_trait::async_trait;
-use fure::policies::{interval, retry_failed};
+use fure::policies::{interval, failed};
 
 pub struct RetryClient<C> {
     client: C,
@@ -27,7 +27,7 @@ impl<C: DnsClient> DnsClient for RetryClient<C> {
     async fn send(&self, query: Query) -> Result<Response> {
         fure::retry(
             || self.client.send(query.clone()),
-            retry_failed(interval(self.next_attempt_delay), self.attempts_count),
+            failed(interval(self.next_attempt_delay), self.attempts_count),
         )
         .await
     }
